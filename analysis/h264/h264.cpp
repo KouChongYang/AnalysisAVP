a880c3db6b2eaea0b1b52672309e53bfe70bdc76
gongluck <1039994845@qq.com>
2020-07-25 15:33:25 +0800

    分割NAL单元

---
+++ analysis/h264/h264.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "h264 analysis" << std::endl;
+
+	std::cout << "Usage : " << "h264 h264file." << std::endl;
+
+	if (argc < 2)
+	{
+		std::cerr << "please see the usage message." << std::endl;
+		return -1;
+	}
+	std::ifstream in(argv[1], std::ios::binary);
+	if (in.fail())
+	{
+		std::cerr << "can not open file " << argv[1] << std::endl;
+		return -1;
+	}
+
+	unsigned char c = 0;
+	uint32_t datalen = 0;
+	int step = 0;//记录0x00的个数
+	while (in.read(reinterpret_cast<char*>(&c), 1))
+	{
+		if (c == 0)
+		{
+			++step;
+		}
+		else if (c == 1 && step >= 2)
+		{
+			datalen += (step > 3 ? step - 3 : 0);
+			if (datalen != 0)
+			{
+				std::cout << "nal size " << datalen << std::endl;
+			}
+			datalen = 0;
+			step = 0;
+		}
+		else
+		{
+			datalen += step + 1;
+			step = 0;
+		}
+	}
+
+	datalen += step;
+	if (datalen != 0)
+	{
+		std::cout << "nal size " << datalen << std::endl;
+	}
+
+	in.close();
+	return 0;
+}
