93585ccfd4533d5d9cbf09e499ba54c2b703d933
gongluck <1039994845@qq.com>
2020-09-12 00:05:33 +0800

    SDL2

--- example/SDL2/main.cpp
+++ example/SDL2/main.cpp
@@ -1,11 +1,13 @@
 ﻿/*
  * @Author: gongluck
  * @Date: 2020-09-11 22:22:14
- * @Last Modified by:   gongluck
- * @Last Modified time: 2020-09-11 22:22:14
+ * @Last Modified by: gongluck
+ * @Last Modified time: 2020-09-12 00:05:16
  */
 
 #define SDL_MAIN_HANDLED
+
+#define MY_QUIT SDL_USEREVENT+1
 
 const int WIDTH = 300;
 const int HEIGHT = 300;
@@ -23,7 +25,7 @@ int main()
 	auto texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, WIDTH, HEIGHT);
 
 	int i = 0;
-	while (i++ < 120)
+	while (i++ < 20)
 	{
 		ret = SDL_SetRenderTarget(renderer, texture);
 		ret = SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -38,6 +40,50 @@ int main()
 
 		SDL_RenderPresent(renderer);
 		SDL_Delay(500);
+	}
+
+	bool exit = false;
+	SDL_Event event = { 0 };
+	while (!exit)
+	{
+		ret = SDL_WaitEvent(&event);
+		switch (event.type)
+		{
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym >= SDLK_a && event.key.keysym.sym <= SDLK_z)
+			{
+				std::cout << char('a' + event.key.keysym.sym - SDLK_a) << " down" << std::endl;
+			}
+			else if (event.key.keysym.sym == SDLK_ESCAPE)
+			{
+				SDL_Event event_q;
+				event_q.type = MY_QUIT;
+				ret = SDL_PushEvent(&event_q);
+				break;
+			}
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+			if (event.button.button == SDL_BUTTON_LEFT)
+			{
+				std::cout << "mouse left down" << std::endl;
+			}
+			else if (event.button.button == SDL_BUTTON_RIGHT)
+			{
+				std::cout << "mouse right down" << std::endl;
+			}
+			else
+			{
+				std::cout << "mouse down" << std::endl;
+			}
+			break;
+		case SDL_MOUSEMOTION:
+			std::cout << "mouse move " << event.button.x << ", " << event.button.y << std::endl;
+			break;
+		case MY_QUIT:
+			std::cout << "my quit envent." << std::endl;
+			exit = true;
+			break;
+		}
 	}
 
 	SDL_DestroyWindow(window);
